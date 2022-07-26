fb12d150e6dffc42a0330908047d2f459287ea19
matheusribeiro02 <contatomatheusrrodrigues@gmail.com>
2022-07-26 14:32:20 -0300

    finalizaSecao8

---
+++ aula91_fetch-axios/assets/js/main.js
@@ -0,0 +1,32 @@
+// fetch('pessoas.json')
+//   .then(resposta => resposta.json())
+//   .then(json => carregaElementoNaPagina(json));
+
+axios('pessoas.json')
+  .then(resposta => carregaElementoNaPagina(resposta.data));
+
+function carregaElementoNaPagina(json) {
+  const table = document.createElement('table');
+
+  for (let pessoa of json) {
+    const tr = document.createElement('tr');
+
+    let td = document.createElement('td');
+    td.innerHTML = pessoa.nome;
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerHTML = pessoa.idade;
+    tr.appendChild(td);
+
+
+    td = document.createElement('td');
+    td.innerHTML = pessoa.salario;
+    tr.appendChild(td);
+
+    table.appendChild(tr);
+  }
+
+  const resultado = document.querySelector('.resultado');
+  resultado.appendChild(table);
+}
